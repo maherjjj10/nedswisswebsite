e3674b61f60aeb8a34564cca49a11c9986ce593a
maherjjj10 <32654772+maherjjj10@users.noreply.github.com>
2025-11-02 02:36:27 +0100

    Create ClientServices.tsx

---
+++ app/[locale]/services/ClientServices.tsx
@@ -0,0 +1,13 @@
+
+'use client'
+import { ServicesTab } from '@/components/Services'
+
+const page = () => {
+  return (
+    <div>
+        <ServicesTab />
+    </div>
+  )
+}
+
+export default page
