76c5d6950a5ee49ac2f0f04eb3182d6b252eaec5
maherjjj10 <32654772+maherjjj10@users.noreply.github.com>
2025-11-02 01:53:03 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,8 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["de", "en", "fr"],
-    defaultLocale: "de"
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ['images.unsplash.com', 'nedsite.runasp.net'],
-  },
+  eslint: { ignoreDuringBuilds: true },
+  images: { domains: ['images.unsplash.com', 'nedsite.runasp.net'] }
 };
 
 export default withNextIntl(nextConfig);
